e7f727b7c7e756fbb4c0b2f16148f07710e81578
Jose Cortes <josehcortes02@gmail.com>
2023-12-04 12:12:00 -0800

    Hey

--- pdftext.js
+++ pdftext.js
@@ -11,7 +11,7 @@ const openai = new OpenAIApi(configuration);
 function formatDate(date) {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
+  const day = String(date.getDate());
   return `${year}-${month}-${day}`;
 }
 
